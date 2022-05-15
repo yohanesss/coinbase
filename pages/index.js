1231450982de5425907a6703fc5dc1e38d8cd6f8
Yohanes Setiawan <test27c@gmail.com>
2022-05-14 19:56:22 -0700

    test netlify deploy

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello World! 🌎</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello World! 🌎 </h1>;
 }
